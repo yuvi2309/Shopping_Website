4e9d8effca22b05072f6adb8ee8e7dfcd0c8f041
Yuvraj2309 <dhakadyuvraj23@gmail.com>
2022-10-16 03:22:38 +0530

    Register more models with some new colomns and editable list

--- store/models.py
+++ store/models.py
@@ -52,6 +52,10 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default='MEMBERSHIP_BRONZE') 
 
+    def __str__(self) -> str:
+        return self.first_name
+        
+
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
