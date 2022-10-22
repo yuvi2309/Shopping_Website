4bb34c9c6d67a954327bc73ad4aa2f3d18ea4651
Yuvraj2309 <dhakadyuvraj23@gmail.com>
2022-10-23 02:13:37 +0530

    Advanced API Concepts Lec-14

--- store/views.py
+++ store/views.py
@@ -1,4 +1,3 @@
-from store.filters import ProductFilter
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
@@ -6,6 +5,8 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from .pagination import DefaultPagination
+from .filters import ProductFilter
 from .models import Collection, Product, Review, OrderItem
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
@@ -16,6 +17,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
